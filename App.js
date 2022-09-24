ed6e670f69e2cb86e943ae3d20d4786e31efc63b
Matt Pham <matthewpham135@gmail.com>
2022-09-24 08:50:17 -0500

    starter file

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ components/index.js
@@ -0,0 +1,16 @@
+
+
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+import { SubInfo } from './SubInfo'
+
+export {
+  HomeHeader, NFTCard, FocusedStatusBar, 
+  CircleButton, RectButton,
+  DetailsDesc, DetailsBid,
+  SubInfo, 
+};
